47197d90f4b4f76def9ec97502a291a0930826cd
Thierry Di Lenarda <cantkillemall@users.noreply.github.com>
2023-08-20 18:22:45 +0200

    Add Single page with UTooltip

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex justify-center items-center h-screen">
+        <UTooltip text="Home"><h1 class="text-9xl font-bold">Home</h1></UTooltip>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
